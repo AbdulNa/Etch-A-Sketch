c235435e6176aeb1f4165ae225619dad45d5350c
AbdulNa <anapaate03an@gmail.com>
2024-01-18 18:22:47 +0000

    working grid

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-const container = document.getElementById('major-square');
+const gridside = 600;
+let rows_Column = 21;
 
-function createDivs(numDivs) {
-    for (let i = 0; i < numDivs; i++) {
-        //creates new elements
-        let innersquare = document.createElement('div');
-        //creates a class for the div
-        innersquare.className = 'square';
+const sketchArea = document.querySelector("#sketch-area");
+sketchArea.style.width = `${gridside}px`;
+sketchArea.style.height = `${gridside}px`;
 
-        container.appendChild(innersquare);
+// funtion to change background when hovered/*
+function backgroundChange() { 
+    if (isMouseDown) { 
+        this.style.backgroundColor = "black";
     }
 }
+function handleMouseDown() { 
+    isMouseDown = true;
+}
+function handleMouseup() { 
+    isMouseDown = false;
+}
+function backgroundChange() {
+    this.style.backgroundColor = "black";
+}
 
-document.getElementById('click').addEventListener('click', function() {
-    return createDivs(256);
-})
+//funtion  for eraser
+function eraser() {
+    if (isMouseDown) {
+        this.style.backgroundColor = "white";
+    }
+}
+function handleMouseDown() {
+    isMouseDown = true;
+}
+function handleMouseup() {
+    isMouseDown = false;
+}
+
+function createCells() {
+    const mini_square = (rows_Column * rows_Column);
+    //loops to create desired mini cubes    
+    for(let i = 0; i < mini_square; i++) {
+        const gridCell = document.createElement("div");
+        gridCell.style.width = `${(gridside / rows_Column)- 2}px`;
+        gridCell.style.height = `${(gridside / rows_Column)- 2}px`;
+        gridCell.classList.add("cell");
+        sketchArea.appendChild(gridCell);
+
+        //gridCell.addEventListener("mousedown", handleMouseDown);        
+        //gridCell.addEventListener("mouseup", handleMouseup);        
+        gridCell.addEventListener("mouseover", backgroundChange);
+    }
+}
+createCells();
